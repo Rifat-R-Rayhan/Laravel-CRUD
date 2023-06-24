5e942e545d1ae9f3a567c185403d102d725aafea
Rifat-R-Rayhan <rifatrayhan2001@gmail.com>
2023-06-24 10:36:42 +0600

    Initial commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,58 @@
+@extends('products.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Laravel CRUD - Rifat 0400038849</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Catagory</th>
+            <th>Brand</th>
+            <th>Description</th>
+            <th>Image SRC</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->catagory_name }}</td>
+            <td>{{ $product->brand_name }}</td>
+            <td>{{ $product->description }}</td>
+            <td>{{ $product->image }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    {!! $products->links() !!}
+
+@endsection
---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,47 @@
+@extends('products.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {{ $product->name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Catagory:</strong>
+                {{ $product->catagory_name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Brand:</strong>
+                {{ $product->brand_name }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                {{ $product->description }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Image SRC:</strong>
+                {{ $product->image }}
+            </div>
+        </div>
+    </div>
+@endsection
